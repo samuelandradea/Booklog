8eb6f5695e8406b7a00a856f74067bf0cb986404
samuelandradea <samuel.aas.2005@gmail.com>
2026-04-20 21:22:21 -0300

    conecta infolivro com API pelo isbn13

--- app/(tabs)/infolivro.tsx
+++ app/(tabs)/infolivro.tsx
@@ -1,6 +1,7 @@
 import { useProtectedRoute } from '@/hook/useProtectedRoute';
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
+import { Header } from '@/components/Header';
 import {
   ActivityIndicator,
   Image,
@@ -56,7 +57,7 @@ export default function LivroInfo() {
     <SafeAreaView style={styles.safeArea} edges={['top', 'bottom']}>
       <ScrollView contentContainerStyle={styles.content}>
 
-        <Text style={styles.header}>booklog</Text>
+        <Header />
 
         <View style={styles.topoContainer}>
           <View style={styles.capa}>
